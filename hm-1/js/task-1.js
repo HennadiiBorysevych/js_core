d9c38237d7dfaff0e0369c7022249199b2e81c4b
Hennadii Borysevych <genuch3@gmail.com>
2023-08-02 16:13:18 +0200

    feat:changes to task 2

--- hm-1/js/task-1.js
+++ hm-1/js/task-1.js
@@ -3,9 +3,12 @@ const ERROR_MESSAGE = 'Invalid value, Enter a number!';
 function toBase() {
 	const number = +prompt('Enter a number, please!');
 	const base = +prompt('Enter a base, please!');
-	return !number && !base
-		? console.log(ERROR_MESSAGE)
-		: console.log(+number.toString(base));
+
+	if (isNaN(firstValue) || isNaN(secondValue)) {
+		console.log(ERROR_MESSAGE);
+	} else {
+		console.log(Number(firstValue).toString(secondValue));
+	}
 }
 
 toBase();
